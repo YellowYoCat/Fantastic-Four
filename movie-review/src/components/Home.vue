3e3d729d1cb347af6c4f1e0caf35912c3dce895d
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-01-15 10:09:47 -0700

    update home
    got rid of the links to the githubs.

--- movie-review/src/components/Home.vue
+++ movie-review/src/components/Home.vue
@@ -4,8 +4,7 @@
     <h3>This will be the nav</h3>
     <ul>
       <router-link to="/Login">Login</router-link>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+
     </ul>
   </div>
 </template>
