3ad598208a2e855f26fa67f301a763d25e90e2dd
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-01-19 11:03:29 -0700

    Login page started

--- movie-review/src/components/Login.vue
+++ movie-review/src/components/Login.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-      <h1>Login</h1>
-      <p>Please log in to continue.</p>
+    <div class="card">
+     <h1>Register</h1>
+     <div>
+      <h4>Email</h4>
+      <input v-model="Text" type="email" placeholder="Enter Email"/>
+      <h4>Password</h4>
+      <input type="password" v-model="password" placeholder="Enter Password"/>
+      <h4>Confirm Password</h4>
+      <input type="password" v-model="password" placeholder="Confirm Password"/>
+      <br>
+      <button class="loginbtn" @click="register">Register</button>
+     </div>
+
     </div>
   </template>
   
   <script>
   export default {
-    name: 'LoginView', 
-  };
+    name: 'LoginView',
+    data(){
+      return{
+        password: ""
+      }
+    }
+};
+
+  
+  
   </script>
   
