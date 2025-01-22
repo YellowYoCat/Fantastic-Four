9f9a0b5363db9fa24fc79e62b1e0270f50a53661
hauntedghoul <madison.mitchell6704@gmail.com>
2025-01-22 08:54:15 -0700

    center
    centered home page

--- movie-review/src/components/Navbar.vue
+++ movie-review/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bg-blue-900 flex items-center justify-between p-4 shadow-lg">
-     ]
+
       <div class="flex items-center">
         <div class="bg-white rounded-full p-2">
           <img src="../assets/logo.png" alt="Logo" class="h-8 w-8">
@@ -13,6 +13,7 @@
         <button class="nav-button">Movies</button>
         <button class="nav-button">Sign Up</button>
         <button class="nav-button">Login</button>
+        <button class="nav-button">About</button>
       </div>
     </nav>
   </template>
