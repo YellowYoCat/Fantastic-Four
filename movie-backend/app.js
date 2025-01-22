e8450a92d8b8ea44a045d9caef1e8bbf472d73be
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-01-22 09:56:24 -0700

    api kay

--- movie-backend/app.js
+++ movie-backend/app.js
@@ -1,9 +1,15 @@
+// Movie api key
+// 320b4a81527cb06be689a396ecc7be50
+
 console.log("is this working?");
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cor());
 
 app.listen(PORT, (e) => {
     if (e) {
@@ -13,7 +19,7 @@ app.listen(PORT, (e) => {
     }
 });
 
-
+app.get('')
 
 
 
