9877cbb40e260ed0cfa171f66135779ca53d1405
hauntedghoul <madison.mitchell6704@gmail.com>
2025-01-27 09:16:40 -0700

    API
    able to get pages up

--- movie-backend/app.js
+++ movie-backend/app.js
@@ -1,15 +1,29 @@
 // Movie api key
 // 320b4a81527cb06be689a396ecc7be50
+// Movie API key
+const MOVIE_API_KEY = "320b4a81527cb06be689a396ecc7be50";
 
 console.log("is this working?");
 
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
-app.use(cor());
+// Middleware
+app.use(cors());
+app.use(bodyParser.json()); // for parsing JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing form submissions
+
+// Routes
+
+// Test the server
+app.get('/', (req, res) => {
+    res.send("Welcome to the Review Everything! Movie API");
+});
 
 app.listen(PORT, (e) => {
     if (e) {
@@ -21,5 +35,5 @@ app.listen(PORT, (e) => {
 
 app.get('')
 
-
+ 
 
