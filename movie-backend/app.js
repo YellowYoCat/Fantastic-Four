c04a83e1effd6610a3a420904c40fb878006c799
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2025-01-27 09:37:59 -0700

    basic routes

--- movie-backend/app.js
+++ movie-backend/app.js
@@ -7,19 +7,49 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
+const apiKey = '320b4a81527cb06be689a396ecc7be50';
 
-app.use(cor());
+app.use(cors({ origin: '*' }));
 
-app.listen(PORT, (e) => {
-    if (e) {
-        console.log("error");
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
     } else {
-        console.log(`server is running on port ${PORT}`);
+        console.log(`Server is running on port ${port}`);
     }
 });
 
-app.get('')
+
+app.get('/movie/:id', async (req, res) => {
+    const id = req.params.id;
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+    }catch{
+        console.log("error");
+    }
+});
+
+
+
+
+const valAPIKey = (req, res, next) => {
+    const apiKey = req.header('api-key');
+    if (apiKey === '320b4a81527cb06be689a396ecc7bew50') {
+        next();
+    } else {
+        res.status(401).send({ error: 'Unauthorized' });
+
+    }
+}
 
 
 
